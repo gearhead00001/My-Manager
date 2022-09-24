ec85475873d62f77318b546ce8231950d7ead930
Nick-YJ <jeffreynicholasy.swe@gmail.com>
2022-09-24 11:03:24 +0530

    Added text component

--- App.tsx
+++ App.tsx
@@ -1,18 +1,17 @@
 /**
  * My Manager app
  */
- 
 
-import React, {type PropsWithChildren} from 'react';
-import { SafeAreaView } from 'react-native';
-import AppStack from './src/navigation/AppStack';
+import React, {type PropsWithChildren} from 'react'
+import { SafeAreaView } from 'react-native'
+import AppStack from './src/navigation/AppStack'
 
 const App = () => {
-  return (
-    <SafeAreaView style={{flex:1}}>
-      <AppStack/>
-    </SafeAreaView>
-  );
-};
+	return (
+		<SafeAreaView style={{flex:1}}>
+			<AppStack/>
+		</SafeAreaView>
+	)
+}
 
-export default App;
+export default App
--- src/components/RcText/RcText.tsx
+++ src/components/RcText/RcText.tsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import styled from 'styled-components/native'
-import { IRcTextProps } from './types'
+import { IRcTextProps, IRcTextStyProps } from './types'
 
-const RcText = (props: IRcTextProps) => {
+const RcText = (props : IRcTextProps) => {
+	const {
+		content,
+		fontSize = 14,
+		fontColor = '#000',
+		fontFamily = 'NotoSans-Regular',
+		fontWeight = 700,
+		textAlign = 'center'
+	} = props
+
 	return (
-		<Text>
-			{props.content}
+		<Text
+			fontSize = {fontSize}
+			fontColor = {fontColor}
+			fontFamily = {fontFamily}
+			fontWeight = {fontWeight}
+			textAlign = {textAlign}
+		>
+			{content}
 		</Text>
 	)
 }
 
-const Text = styled.Text``
+const Text = styled.Text<IRcTextStyProps>`
+	font-size : ${p => p.fontSize}px;
+	color :${p => p.fontColor};
+	font-family : ${p => p.fontFamily};
+	font-weight: ${p => p.fontWeight};
+	text-align: ${p => p.textAlign}
+
+`
 
 export default RcText
